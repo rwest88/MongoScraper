0f6b409474bf3474e21aa74ff09f5c8f96df8ba8
Ryan Weston <rwest88.va@gmail.com>
2018-06-08 19:59:53 -0400

    built server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(router);
+
+// If deployed, use the deployed database, otherwise use the local database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+
+mongoose.connect(db, function(err) {
+  if (err) return console.log(err);
+  console.log("mongoose connection successful")
+})
+
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
+});
